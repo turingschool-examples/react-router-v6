a343f95c71d21900c8c5b49f415b2eed9a5b4efc
Kayla Wood <kaylaewood@gmail.com>
2021-12-06 12:18:46 -0700

    Fix creature details

--- src/Components/CreatureDetails/CreatureDetails.js
+++ src/Components/CreatureDetails/CreatureDetails.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './CreatureDetails.css';
 
-const CreatureDetails = ({ creatureType, creatureId }) => {
+const CreatureDetails = () => {
   return (
     <div>
-      <h1>{`${creatureType.toUpperCase()}: ${creatureId}`}</h1>
+      <h1>Creature Details!</h1>
     </div>
   )
 }
