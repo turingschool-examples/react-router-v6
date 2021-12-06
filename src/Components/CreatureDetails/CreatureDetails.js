6c59fb0a3653a46d3a96cf7f53672357773e5c5e
Kayla Wood <kaylaewood@gmail.com>
2021-12-06 12:30:19 -0700

    Complete app

--- src/Components/CreatureDetails/CreatureDetails.js
+++ src/Components/CreatureDetails/CreatureDetails.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './CreatureDetails.css';
+import { useParams } from 'react-router-dom';
+import { getCreatureDetails } from '../../data/animalData';
 
 const CreatureDetails = () => {
+  const creatureType = useParams().animal;
+  const creatureId = useParams().id;
+
+  const creatureStats = getCreatureDetails(creatureType, creatureId);
 
   return (
     <div>
-      <h1>Some Creature Name</h1>
-      <img src='some_image_url' className='app-img-no-hover'/>
-      <p className='creature-bio'>Some bio about this creature</p>
+      <h1>{creatureStats.name}</h1>
+      <img src={creatureStats.image} className='app-img-no-hover'/>
+      <p className='creature-bio'>{creatureStats.bio}</p>
     </div>
   )
 }
