d3397c19e1de8cca97203f1f35efae7de94267eb
Kayla Wood <kaylaewood@gmail.com>
2021-12-06 12:22:53 -0700

    Add more skeleton to creature details

--- src/Components/CreatureDetails/CreatureDetails.js
+++ src/Components/CreatureDetails/CreatureDetails.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './CreatureDetails.css';
 
 const CreatureDetails = () => {
+
   return (
     <div>
-      <h1>Creature Details!</h1>
+      <h1>Some Creature Name</h1>
+      <img src='some_image_url' className='app-img-no-hover'/>
+      <p className='creature-bio'>Some bio about this creature</p>
     </div>
   )
 }
